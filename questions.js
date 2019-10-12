69710ac70cb69a8660f1d8b7ac22ca1f84e91ad1
Norman Lei <normanlei0901@gmail.com>
2019-10-12 02:05:38 -0700

    update hw

--- questions.js
+++ questions.js
@@ -20,7 +20,12 @@ var quizPage = document.getElementById("quizpage");
 var initialenterpage = document.getElementById("scoreenterpage");
 var finalPage = document.getElementById("recordpage");
 var header = document.getElementById("header");
-
+var correctSound = document.getElementById("correct");
+var wrongSound = document.getElementById("wrong");
+var startSound = document.getElementById("startSound");
+var endingSound = document.getElementById("endingSound");
+$("#friend-summary").hide();
+$("#newfriend-summary").hide();
 var questionCollection = [
     {
         title: "Where do I come from?",
@@ -59,10 +64,16 @@ var timeCountDown;
 var score = 0;
 var record = [];
 var tempRecord = JSON.parse(localStorage.getItem("data"));
-if (tempRecord!==null) record = tempRecord;
+if (tempRecord !== null) record = tempRecord;
 else localStorage.setItem("data", JSON.stringify(record));
 
-viewScorebtn.addEventListener("click",function(){
+HTMLAudioElement.prototype.stop = function () {
+    this.pause();
+    this.currentTime = 0.0;
+}
+
+
+viewScorebtn.addEventListener("click", function () {
     renderRecorePage();
 });
 
@@ -84,23 +95,24 @@ choices.addEventListener("click", function (event) {
 submitbtn.addEventListener("click", submitScore);
 
 gobackbtn.addEventListener("click", function () {
-    //window.location.reload();
+    window.location.reload();
     //window.history.go(-1);
-    landingPage.style.display = "block";
-    quizPage.style.display = "none";
-    initialenterpage.style.display = "none";
-    header.style.display = "block";
-    finalPage.style.display = "none";
-    scoreList.innerHTML = "";
+    // landingPage.style.display = "block";
+    // quizPage.style.display = "none";
+    // initialenterpage.style.display = "none";
+    // header.style.display = "block";
+    // finalPage.style.display = "none";
+    // scoreList.innerHTML = "";
 });
 
-clearbtn.addEventListener("click",function(){
+clearbtn.addEventListener("click", function () {
     record = [];
     localStorage.setItem("data", JSON.stringify(record));
     scoreList.innerHTML = "";
 });
 
 function startQuiz() {
+    startSound.play();
     landingPage.style.display = "none";
     quizPage.style.display = "block";
     isCorrect = null;
@@ -120,17 +132,25 @@ function timeCount() {
 }
 
 function renderQuestion() {
+    correctSound.stop();
+    wrongSound.stop();
     var tempQuestion = questionCollection[index];
     questionTitle.innerHTML = tempQuestion.title;
     choiceA.innerHTML = "A. " + tempQuestion.choices[0];
     choiceB.innerHTML = "B. " + tempQuestion.choices[1];
     choiceC.innerHTML = "C. " + tempQuestion.choices[2];
     choiceD.innerHTML = "D. " + tempQuestion.choices[3];
-    if (isCorrect) showAns.innerHTML = "<hr> <h6>Correct!!!</h6>";
-    else if (isCorrect === false) showAns.innerHTML = "<hr> <h6>Wrong!!!</h6>";
+    if (isCorrect) {
+        showAns.innerHTML = "<hr> <h6>Correct!!!</h6>";
+        correctSound.play();
+    }
+    else if (isCorrect === false) {
+        showAns.innerHTML = "<hr> <h6>Wrong!!!</h6>";
+        wrongSound.play();
+    }
     setTimeout(function () {
         showAns.innerHTML = "";
-    }, 2000);
+    }, 1500);
     index++;
 }
 
@@ -161,12 +181,24 @@ function finishQuiz() {
     initialenterpage.style.display = "block";
     score = timeLeft + score * 5;
     finalScore.innerHTML = " " + score;
-    if (isCorrect===true) showAns1.innerHTML = "<hr> <h6>Correct!!!</h6>";
-    else if (isCorrect === false) showAns1.innerHTML = "<hr> <h6>Wrong!!!</h6>";
-    else showAns1.innerHTML = "<hr> <h6>Time's Out!!!</h6>";
+    if (score >= 40) {
+        $("#friend-summary").show(1000);
+    } else $("#newfriend-summary").show(1000);
+    if (isCorrect === true) {
+        showAns1.innerHTML = "<hr> <h6>Correct!!!</h6>";
+        correctSound.play();
+    }
+    else if (isCorrect === false) {
+        showAns1.innerHTML = "<hr> <h6>Wrong!!!</h6>";
+        wrongSound.play();
+    }
+    else {
+        showAns1.innerHTML = "<hr> <h6>Time's Out!!!</h6>";
+        wrongSound.play();
+    }
     setTimeout(function () {
         showAns1.innerHTML = "";
-    }, 2000);
+    }, 1500);
 }
 
 function submitScore() {
@@ -174,25 +206,26 @@ function submitScore() {
         alert("Please Enter Your Valid Initial.");
         return;
     }
-    else{
-    var initialInput = initial.value.trim();
-    initial.value = "";
-    var quizRecord = {
-        initialName: initialInput,
-        score: score
-    };
-    record = JSON.parse(localStorage.getItem("data"));
-    record.push(quizRecord);
-    localStorage.setItem("data", JSON.stringify(record));
-    score = 0;
-    isCorrect = null;
-    timeLeft = totalTime;
-    shownTime.innerHTML = "0 s";
-    renderRecorePage();
+    else {
+        var initialInput = initial.value.trim();
+        initial.value = "";
+        var quizRecord = {
+            initialName: initialInput,
+            score: score
+        };
+        record = JSON.parse(localStorage.getItem("data"));
+        record.push(quizRecord);
+        localStorage.setItem("data", JSON.stringify(record));
+        score = 0;
+        isCorrect = null;
+        timeLeft = totalTime;
+        shownTime.innerHTML = "0 s";
+        renderRecorePage();
     }
 }
 
 function renderRecorePage() {
+    endingSound.play();
     landingPage.style.display = "none";
     quizPage.style.display = "none";
     initialenterpage.style.display = "none";
@@ -203,8 +236,21 @@ function renderRecorePage() {
     //localStorage.setItem("data", JSON.stringify(record));
     for (var i = 0; i < record.length; i++) {
         var currentScore = document.createElement("div");
-        currentScore.textContent = record[i].initialName + " - " + record[i].score;
+        if (i === 0) {
+            currentScore.textContent = "🏅" + (i + 1) + ". " + record[i].initialName + " - " + record[i].score;
+            currentScore.style.backgroundColor = "gold";
+        } else if (i === 1) {
+            currentScore.textContent = "🥈" + (i + 1) + ". " + record[i].initialName + " - " + record[i].score;
+            currentScore.style.backgroundColor = "silver";
+        } else if (i === 2) {
+            currentScore.textContent = "🥉" + (i + 1) + ". " + record[i].initialName + " - " + record[i].score;
+            currentScore.style.backgroundColor = "#b87333"; //copper
+        } else {
+            currentScore.textContent = (i + 1) + ". " + record[i].initialName + " - " + record[i].score;
+            currentScore.style.backgroundColor = "green";
+        }
         scoreList.appendChild(currentScore);
+        currentScore.style.marginBottom = "10px";
     }
 }
 
